cd58de6ff42711f6d8fdff8f0ac1b57575701e6f
Dmitriy Canishev <canishev.dima2019@gmail.com>
2025-03-03 17:00:40 +0300

    Create Spin scene, Create Spin Logic scripts and folder under it, Add sprites for example of spinning wheel

---
+++ Assets/Scripts/App/Base/SubviewController.cs
@@ -0,0 +1,21 @@
+﻿namespace App.Base
+{
+    public class SubviewController<TParentView> : ViewController where TParentView : ViewController
+    {
+        private TParentView _parentView = null;
+
+        protected TParentView ParentView { get; private set; }
+
+        protected override void Bind()
+        {
+            base.Bind();
+            ParentView = _parentView ??= GetComponentInParent<TParentView>();
+        }
+
+        protected override void Unbind()
+        {
+            base.Unbind();
+            ParentView = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/CollectionUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utils
+{
+    public static class CollectionUtils
+    {
+        public static T WeightedRandom<T>(this IEnumerable<T> enumerable, Func<T, float> weightSelector)
+        {
+            var random = Random.Range(0, (int) enumerable.Sum(weightSelector));
+            Debug.LogError($"WeightRandom = {random}");
+            var weightSum = 0f;
+            foreach (var item in enumerable)
+            {
+                var add = weightSelector.Invoke(item);
+                Debug.LogError($"WeightSelectorInvoke {add}");
+                weightSum += add;
+                if (weightSum >= random)
+                {
+                    Debug.LogError($"WeightSum = {weightSum}");
+                    return item;
+                }
+            }
+
+            throw new ArgumentException("Impossible error!");
+        }
+    }
+}
